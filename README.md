768a7b3658b69745e800257fdb1e81f288115d1f
julialotta <julialottatinglof@gmail.com>
2022-02-09 16:16:59 +0100

    Update in progress with routers and mongoDB

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
 # MI-DW-NODEJS-TodoList
 
+---
+
 ## Assignment in the course Dynamic Web Development
 
-### Assignment: 
+---
+
+### Assignment:
+
 - Create a TODO application with Express and Handlebars.
 - All Todo tasks should have the following attributes:
-	-  Id
-	-  Created (date & time)
-	-   Description
-	-   Done
+  - Id
+  - Created (date & time)
+  - Description
+  - Done
 - User should be able to perform all the CRUD operations (Create, Read, Update & Delete) on the tasks.
 
 - User can chose to show all tasks, only done tasks (done=true) or not done tasks (done=false).
 - User can sort tasks after creation date, both newest → oldest and oldest → newest.
 
+### Added after the assignment was done for fun and practice:
+
+- Some fun styling
+- Implemented express routers
+- Implemented MongoDB database
---
+++ routers/task-router.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const router = express.Router();
+const getDb = require("../database.js");
+const getDate = require("../lib/getDate.js");
+/* const { ObjectId } = require("mongodb"); */
+
+const COLLECTION_NAME = "todos";
+
+router.get("/", async (req, res) => {
+  const db = await getDb();
+  const dbTodos = db.collection(COLLECTION_NAME).find();
+
+  const todos = [];
+
+  await dbTodos.forEach((task) => {
+    todos.push(task);
+  });
+  res.render("home", { todos });
+});
+
+/* 
+router.get("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const task = todos.find((i) => i.id === id);
+  res.render("single-task", task);
+});
+
+router.get("/uncompletedtasks", (req, res) => {
+  res.render("uncompleted-tasks", { todos });
+});
+
+router.get("/completedtasks", (req, res) => {
+  res.render("completed-tasks", { todos });
+});
+
+router.get("/descending", (req, res) => {
+  todos.sort((a, b) => (a.created > b.created ? 1 : -1));
+  res.render("home", { todos });
+});
+
+router.get("/ascending", (req, res) => {
+  todos.sort((a, b) => (a.created > b.created ? -1 : 1));
+  res.render("home", { todos });
+}); */
+
+router.post("/newtask", async (req, res) => {
+  const date = getDate();
+  if (req.body.description) {
+    const newTodo = {
+      created: date,
+      description: req.body.description,
+      done: false,
+    };
+    const db = await getDb();
+    await db.collection(COLLECTION_NAME).insertOne(newTodo);
+    res.sendStatus(201);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+router.get("/:id/edit", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = todos.find((i) => i.id === id);
+  res.render("edit", index);
+});
+
+router.post("/:id/edit", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = todos.findIndex((i) => i.id === id);
+  todos[index].created = req.body.created.replace("T", " ");
+  todos[index].description = req.body.description;
+  if (req.body.done) {
+    todos[index].done = true;
+  } else {
+    todos[index].done = false;
+  }
+  res.redirect("/" + id);
+});
+
+router.get("/:id/delete", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = todos.find((i) => i.id === id);
+  res.render("delete", index);
+});
+
+router.post("/:id/delete", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = todos.findIndex((i) => i.id === id);
+  todos.splice(index, 1);
+  res.redirect("/");
+});
+
+module.exports = router;
