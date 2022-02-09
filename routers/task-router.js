8487ed41bb4e356750bcdde6e1b8ae23d99785fe
julialotta <julialottatinglof@gmail.com>
2022-02-09 20:22:42 +0100

    Added edit and delete via mongodb

--- routers/task-router.js
+++ routers/task-router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const getDb = require("../database.js");
 const getDate = require("../lib/getDate.js");
 const { ObjectId } = require("mongodb");
+const req = require("express/lib/request");
 
 const COLLECTION_NAME = "todos";
 
@@ -18,23 +19,38 @@ router.get("/", async (req, res) => {
   res.render("home", { todos });
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/task/:id", async (req, res) => {
   const id = ObjectId(req.params.id);
   const db = await getDb();
   db.collection(COLLECTION_NAME).findOne({ _id: id }, (err, task) => {
-    res.render("single-task", { task });
+    res.render("single-task", task);
   });
 });
 
+router.get("/uncompletedtasks", async (req, res) => {
+  const db = await getDb();
+  const dbTodos = db.collection(COLLECTION_NAME).find();
+
+  const uncompletedTasks = [];
+
+  await dbTodos.forEach((task) => {
+    uncompletedTasks.push(task);
+  });
+  res.render("uncompleted-tasks", { uncompletedTasks });
+});
+
+router.get("/completedtasks", async (req, res) => {
+  const db = await getDb();
+  const dbTodos = db.collection(COLLECTION_NAME).find();
+
+  const completedTasks = [];
+
+  await dbTodos.forEach((task) => {
+    completedTasks.push(task);
+  });
+  res.render("completed-tasks", { completedTasks });
+});
 /* 
-router.get("/uncompletedtasks", (req, res) => {
-  res.render("uncompleted-tasks", { todos });
-});
-
-router.get("/completedtasks", (req, res) => {
-  res.render("completed-tasks", { todos });
-});
-
 router.get("/descending", (req, res) => {
   todos.sort((a, b) => (a.created > b.created ? 1 : -1));
   res.render("home", { todos });
@@ -61,36 +77,45 @@ router.post("/newtask", async (req, res) => {
   }
 });
 
-router.get("/:id/edit", (req, res) => {
-  const id = parseInt(req.params.id);
-  const index = todos.find((i) => i.id === id);
-  res.render("edit", index);
+router.get("/:id/edit", async (req, res) => {
+  const id = ObjectId(req.params.id);
+  const db = await getDb();
+  db.collection(COLLECTION_NAME).findOne({ _id: id }, (err, task) => {
+    res.render("edit", task);
+  });
 });
 
-router.post("/:id/edit", (req, res) => {
-  const id = parseInt(req.params.id);
-  const index = todos.findIndex((i) => i.id === id);
-  todos[index].created = req.body.created.replace("T", " ");
-  todos[index].description = req.body.description;
-  if (req.body.done) {
-    todos[index].done = true;
-  } else {
-    todos[index].done = false;
+router.post("/:id/edit", async (req, res) => {
+  const id = ObjectId(req.params.id);
+
+  if (req.body.description && req.body.created) {
+    const newTask = {
+      description: req.body.description,
+      created: req.body.created.replace("T", " "),
+      done: req.body.done,
+    };
+    const db = await getDb();
+    db.collection(COLLECTION_NAME).updateOne({ _id: id }, { $set: newTask });
+    res.redirect("/");
   }
-  res.redirect("/" + id);
 });
 
-router.get("/:id/delete", (req, res) => {
-  const id = parseInt(req.params.id);
-  const index = todos.find((i) => i.id === id);
-  res.render("delete", index);
+router.get("/:id/delete", async (req, res) => {
+  const id = ObjectId(req.params.id);
+
+  const db = await getDb();
+  db.collection(COLLECTION_NAME).findOne({ _id: id }, (err, task) => {
+    res.render("delete", task);
+  });
 });
 
-router.post("/:id/delete", (req, res) => {
-  const id = parseInt(req.params.id);
-  const index = todos.findIndex((i) => i.id === id);
-  todos.splice(index, 1);
-  res.redirect("/");
+router.post("/:id/delete", async (req, res) => {
+  const id = ObjectId(req.params.id);
+
+  const db = await getDb();
+  db.collection(COLLECTION_NAME).deleteOne({ _id: id }, (err, task) => {
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
